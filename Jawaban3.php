ef9cca1452e32de8ccba99216f3b3939a73dc115
Bagus Indra Prastya <145307320+MRforce19@users.noreply.github.com>
2024-01-25 18:17:37 +0700

    Delete Jawaban3.php

--- Jawaban3.php
+++
@@ -1,9 +0,0 @@
-$categoryName = 'POCO X3 NFC'; // Ambil dari input form
-$categorySlug = Str::slug($categoryName); // Ambil dari input form
-$categoryActive = true; // Ambil dari input form
-
-$category = new Category;
-$category->name = $categoryName;
-$category->slug = $categorySlug;
-$category->active = $categoryActive;
-$category->save();
